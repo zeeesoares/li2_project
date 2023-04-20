a22910bd9ee068854090077633c14889f0417502
Nuno Melo <a104446@alunos.uminho.pt>
2023-04-20 09:36:32 +0100

    Add ex mapa, rm tutorial

--- src/map.c
+++ src/map.c
@@ -3,9 +3,18 @@
 int mapSetUp() {
     char map[10][10];
 
+
+    for(int i=12;i<20;i++){
+        for(int j=12;j<30;j++){
+            mvprintw(i,j,"#");    
+        }
+    }
+        
+
     mvprintw(5,180, "HP: 100/100");
     mvprintw(6,180, "CHAKRA: 82/100");
     mvprintw(7,180, "XP: 436");
     mvprintw(8,180, "LVL: 27");
 	return 0;
 }
+
