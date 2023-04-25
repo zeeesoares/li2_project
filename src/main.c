d943a7255c035dfdc64e1221c9eb2b346957e59f
Diogo Outeiro <a104092@alunos.uminho.pt>
2023-04-25 11:28:29 +0100

    diogo

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <rogue.h>
+#include <ncurses.h>
 
 int ncursesSetUp() {
 	initscr();
 	curs_set(0);
+	start_color;
+	init_pair (1,COLOR_RED,COLOR_BLACK);
+	attron(COLOR_PAIR(1));
 	printw("Hello World!");
+	attroff(COLOR_PAIR(1));
 	noecho();
 	refresh();
 
--- src/map.c
+++ src/map.c
@@ -4,6 +4,7 @@ int mapSetUp() {
     for(int i=5;i<45;i++){
         for(int j=5;j<140;j++){
             if (i == 5 || i == 44 || j == 5 || j == 139) {
+
                 mvprintw(i,j,"#");
                 //map[i][j] = '#';
             }
