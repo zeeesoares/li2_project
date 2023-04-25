5195a5769d270ae70d714cfa964249df6e574c2e
José Soares <a103995@alunos.uminho.pt>
2023-04-25 12:23:52 +0100

    map

--- src/map.c
+++ src/map.c
@@ -1,17 +1,33 @@
 #include <rogue.h>
 
 int mapSetUp() {
-    for(int i=5;i<45;i++){
-        for(int j=5;j<140;j++){
-            if (i == 5 || i == 44 || j == 5 || j == 139) {
-                mvprintw(i,j,"#");
-                //map[i][j] = '#';
-            }
-            else {
-                mvprintw(i,j,".");
-                //map[i][j] = '.';
+    int rows = 40;
+    int cols = 135;
+
+    // alocar memória para o array bidimensional
+    char **map = (char **) malloc(rows * sizeof(char *));
+    for(int i = 0; i < rows; i++) {
+        map[i] = (char *) malloc(cols * sizeof(char));
+    }
+
+    // preencher o array bidimensional com caracteres aleatórios
+    int limit = RAND_MAX / 2;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            int r = rand();
+            if(r < limit) {
+                map[i][j] = '#';
+            } else {
+                map[i][j] = '.';
             }
         }
     }
-	return 0;
+
+    // imprimir o mapa
+    initscr();
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            mvprintw(i+5, j+5, "%c", map[i][j]);
+        }
+    }
 }
