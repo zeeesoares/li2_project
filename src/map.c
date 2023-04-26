334eca28e2e46b9b751e4a78a10a445c536f5626
Nuno Melo <a104446@alunos.uminho.pt>
2023-04-26 15:00:23 +0100

    Nuno

--- src/map.c
+++ src/map.c
@@ -13,7 +13,7 @@ char ** mapSetUp() {
 
     // preencher o array bidimensional com caracteres aleatórios
     int count = 0;
-
+    
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             if ((i == 0)| (j == 0)| (i == 39) | (j == 134)) {
